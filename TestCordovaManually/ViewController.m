62cb66b083d6da855ad8b1d803e9a9037f60b1e8
Dawn <icy19882006@163.com>
2017-11-21 13:37:34 +0800

    Initial Commit

---
+++ TestCordovaManually/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestCordovaManually
+//
+//  Created by Dawn Wang on 2017/11/21.
+//  Copyright © 2017年 Dawn Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TestCordovaManually/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestCordovaManually
+//
+//  Created by Dawn Wang on 2017/11/21.
+//  Copyright © 2017年 Dawn Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
